59879d52439745e906847adea96d6e06ba1147a7
Mitch Myburgh <mitchmyburgh@gmail.com>
2024-10-09 07:43:23 +0200

    add test

--- size_overflow_test.go
+++ size_overflow_test.go
@@ -1,6 +1,8 @@
 package flex
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNested_overflowing_child(t *testing.T) {
 	config := NewConfig()
@@ -149,5 +151,56 @@ func TestParent_wrap_child_size_overflowing_parent(t *testing.T) {
 	assertFloatEqual(t, 0, rootChild0Child0.LayoutGetLeft())
 	assertFloatEqual(t, 0, rootChild0Child0.LayoutGetTop())
 	assertFloatEqual(t, 100, rootChild0Child0.LayoutGetWidth())
+	assertFloatEqual(t, 200, rootChild0Child0.LayoutGetHeight())
+}
+
+func TestNested_overflowing_child_with_overflow_hidden(t *testing.T) {
+	config := NewConfig()
+
+	root := NewNodeWithConfig(config)
+	root.StyleSetMaxWidth(100)
+	root.StyleSetMaxHeight(100)
+	root.StyleSetOverflow(OverflowHidden)
+
+	rootChild0 := NewNodeWithConfig(config)
+	root.InsertChild(rootChild0, 0)
+
+	rootChild0Child0 := NewNodeWithConfig(config)
+	rootChild0Child0.StyleSetWidth(200)
+	rootChild0Child0.StyleSetHeight(200)
+	rootChild0Child0.StyleSetOverflow(OverflowHidden)
+	rootChild0.InsertChild(rootChild0Child0, 0)
+	CalculateLayout(root, Undefined, Undefined, DirectionLTR)
+
+	assertFloatEqual(t, 0, root.LayoutGetLeft())
+	assertFloatEqual(t, 0, root.LayoutGetTop())
+	assertFloatEqual(t, 100, root.LayoutGetWidth())
+	assertFloatEqual(t, 100, root.LayoutGetHeight())
+
+	assertFloatEqual(t, 0, rootChild0.LayoutGetLeft())
+	assertFloatEqual(t, 0, rootChild0.LayoutGetTop())
+	assertFloatEqual(t, 100, rootChild0.LayoutGetWidth())
+	assertFloatEqual(t, 200, rootChild0.LayoutGetHeight())
+
+	assertFloatEqual(t, 0, rootChild0Child0.LayoutGetLeft())
+	assertFloatEqual(t, 0, rootChild0Child0.LayoutGetTop())
+	assertFloatEqual(t, 200, rootChild0Child0.LayoutGetWidth())
+	assertFloatEqual(t, 200, rootChild0Child0.LayoutGetHeight())
+
+	CalculateLayout(root, Undefined, Undefined, DirectionRTL)
+
+	assertFloatEqual(t, 0, root.LayoutGetLeft())
+	assertFloatEqual(t, 0, root.LayoutGetTop())
+	assertFloatEqual(t, 100, root.LayoutGetWidth())
+	assertFloatEqual(t, 100, root.LayoutGetHeight())
+
+	assertFloatEqual(t, 0, rootChild0.LayoutGetLeft())
+	assertFloatEqual(t, 0, rootChild0.LayoutGetTop())
+	assertFloatEqual(t, 100, rootChild0.LayoutGetWidth())
+	assertFloatEqual(t, 200, rootChild0.LayoutGetHeight())
+
+	assertFloatEqual(t, -100, rootChild0Child0.LayoutGetLeft())
+	assertFloatEqual(t, 0, rootChild0Child0.LayoutGetTop())
+	assertFloatEqual(t, 200, rootChild0Child0.LayoutGetWidth())
 	assertFloatEqual(t, 200, rootChild0Child0.LayoutGetHeight())
 }
